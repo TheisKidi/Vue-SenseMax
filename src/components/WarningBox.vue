438b4c43c9c674dffbea74e558d7ac3c3bb033e2
TheisKidi <theis.k@hotmail.com>
2023-12-13 10:12:01 +0100

    komponent warning

---
+++ src/components/WarningBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="top-gap warning-box-container">
+    <div class="warning-box-content">
+      <div class="warning-box-content-top">
+        <img src="../media/Warings.png" />
+        <p>Antal advarsler i dag</p>
+      </div>
+      <div class="warning-box-content-mid">
+        <p>{{ tickets.length }} advarsler</p>
+      </div>
+      <div class="warning-box-content-bot">
+        <p>Se tidligere advarsler</p>
+        <img src="../media/arrowRightVector 1.png" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
