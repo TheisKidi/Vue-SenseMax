e1f4f2d8cfc086c76da596ba239d1c5069e2d170
hasse <hass2699@edu.zealand.dk>
2023-12-13 13:10:27 +0100

    Moved font styling to global sheet

--- src/components/CriticalAreas.vue
+++ src/components/CriticalAreas.vue
@@ -23,53 +23,14 @@
 
 <style>
 /* FONT STYLING */
-a {
-    text-decoration: none;
-}
-
-h2.subtitle {
-    color: #1D1A22;
-    font-family: Poppins;
-    font-size: 1.25rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-}
 
 h2.h2-ticket {
     width: 85%;
 }
 
-h3.subtitle-text {
-    color: #77767A;
-    font-family: Poppins;
-    font-size: 1rem;
-    font-style: normal;
-    font-weight: 500;
-    line-height: normal;
-}
-
 .h3-ticket {
     text-align: right;
     width: 15%;
-}
-
-h4.h4-subtitle {
-    color: #000;
-    font-family: Poppins;
-    font-size: 1.1rem;
-    font-style: normal;
-    font-weight: 500;
-    line-height: normal;
-}
-
-h5.h5-text {
-    color: #77767A;
-    font-family: Poppins;
-    font-size: 0.8rem;
-    font-style: normal;
-    font-weight: 500;
-    line-height: normal;
 }
 
 /* CONTAINER STYLE */
