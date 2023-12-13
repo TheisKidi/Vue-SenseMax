5ababfb668c769ed91118be4656055f816df53c5
TheisKidi <theis.k@hotmail.com>
2023-12-13 11:14:25 +0100

    nye comp

---
+++ src/components/DashboardComp.vue
@@ -0,0 +1,20 @@
+<template>
+  <WarningBox>
+  </WarningBox>
+  <CriticalAreas>
+  </CriticalAreas>
+
+</template>
+
+<script>
+  import WarningBox from './WarningBox.vue';
+  import CriticalAreas from './CriticalAreas.vue';
+
+  export default {
+    components: {
+      WarningBox,
+      CriticalAreas
+    },
+  }
+
+</script>
