18f533be9a6efd730b3401c0ca2bb17464860b3e
hasse <hass2699@edu.zealand.dk>
2023-12-13 09:39:30 +0100

    Added Banner

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        <h1>{{ page.pageTitle }}</h1>
+    <div class="banner-container">
+    </div>
+    <div class="pag-container">
+        <div class="page-title-container">
+            <h1 class="page-title">{{ page.pageTitle }}</h1>
+        </div>
     </div>
 </template>
 
