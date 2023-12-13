1ac430bd4e5a9d3b158a79a6afb79936a9131ce3
TheisKidi <theis.k@hotmail.com>
2023-12-13 13:37:39 +0100

    all

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -35,16 +35,6 @@ export default {
     margin: 0;
 }
 
-.banner-container {
-    background-image: url("../media/banner.png");
-    background-color: rgba(75, 31, 31, 0.4);
-    background-blend-mode: multiply;
-    background-size: cover;
-    height: 10vh;
-    z-index: 1;
-    position: relative; /* Add this line */
-}
-
 .page-container {
     width: 78vw;
     margin-left: auto;
